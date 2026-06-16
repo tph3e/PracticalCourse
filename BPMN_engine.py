78b28361ce058d84bfa6e44d778645c6cff063d9
cangu <go34bef@mytum.de>
2026-06-16 17:43:06 +0200

    Adding Arrival- and BPMNEngines as separate files

---
+++ BPMN_engine.py
@@ -0,0 +1,60 @@
+import pm4py
+from pm4py.objects.petri_net.utils import petri_utils
+from pm4py.objects.petri_net.semantics import is_enabled, execute, enabled_transitions
+from pm4py.objects.petri_net.obj import PetriNet, Marking
+
+class BPMNEngine:
+    def __init__(self):
+        self.case_markings = {}
+        self.model_filename = "model_heuristic.bpmn"
+
+        try:
+            bpmn_graph = pm4py.read_bpmn(self.model_filename)
+            self.net, self.init_marking, self.final_marking = pm4py.convert_to_petri_net(bpmn_graph)
+            print("[BPMNEngine] Model successfully loaded and converted to Petri net.")
+        except Exception as e:
+            print(f"[BPMNEngine] Fallback: Failed to load model due to error: {e}")
+            from pm4py.objects.petri_net.obj import PetriNet, Marking
+            self.net = PetriNet("Safe Fallback")
+            self.init_marking = Marking()
+            self.final_marking = Marking()
+
+    def initialize_case(self, case_id):
+        from copy import copy
+        self.case_markings[case_id] = copy(self.init_marking)
+
+    def getStartActivity(self, data=None):
+        for transition in self.net.transitions:
+            if transition.label is not None:
+                if is_enabled(transition, self.net, self.init_marking):
+                    return transition.label
+        return None
+    
+    def getPossibleNextActivities(self, current_activity, case_id = None) -> list:
+        if case_id is None:
+            return [self.getStartActivity()]
+        if case_id not in self.case_markings:
+            self.initialize_case(case_id)
+
+        current_marking = self.case_markings[case_id]
+        possible_next = []
+
+        for transition in self.net.transitions:
+            if transition.label is not None:
+                if is_enabled(transition,self.net, current_marking):
+                    possible_next.append(transition.label)
+        return possible_next
+    
+    def fire_activity(self, activity_name, case_id) -> bool:
+        if case_id not in self.case_markings:
+            self.initialize_case(case_id)
+
+        current_marking = self.case_markings[case_id]
+        for transition in self.net.transitions:
+            if transition.label == activity_name:
+                if is_enabled(transition,self.net, current_marking):
+                    new_marking = execute(transition,self.net, current_marking)
+                    self.case_markings[case_id] = new_marking
+                    return True
+                
+        return False
